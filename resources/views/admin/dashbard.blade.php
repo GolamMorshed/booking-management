41af3b3a884eb708167466cbcabdc52d09abebff
Golam Morshed <45533293+GolamMorshed@users.noreply.github.com>
2023-06-13 20:23:29 +0100

    bootstrap installation

---
+++ resources/views/admin/dashbard.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Dashboard-Admin</title>
+</head>
+<body>
+    
+</body>
+</html>
